bf9d6b956040efeae0d7885378b04a3af53b5ce4
Vipin9794 <rikyverma112@gmail.com>
2024-10-19 22:14:10 +0530

    resetup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Toaster } from 'sonner'
-import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-    
-    <Toaster/>
+    <App />
   </StrictMode>,
 )
