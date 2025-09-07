ec35b3e93063722d934de3e326fad13ea26d957c
VIPIN KUMAR <143281757+Vipin9794@users.noreply.github.com>
2025-09-08 01:07:11 +0530

    Update LatestJobCard.jsx

--- frontend/src/components/LatestJobCard.jsx
+++ frontend/src/components/LatestJobCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 import { Badge } from "@/components/ui/badge";
 
 const LatestJobCard = ({ job }) => {
